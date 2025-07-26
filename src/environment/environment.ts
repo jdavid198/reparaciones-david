84b8d051d7848bc84dfb96ab12aea16c585deeda
jdavid198 <juan.david1145@gmail.com>
2025-07-26 16:14:26 -0500

    environmets

---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "Values",
+        authDomain: "Values",
+        projectId: "Values",
+        storageBucket: "Values",
+        messagingSenderId: "Values",
+        appId: "Values"
+    }
+  };
+  
