abb17db10880c7aa312f9654c9cef13624bc7137
Stiven Arboleda <stiven2201@gmail.com>
2025-08-02 10:30:10 -0500

    Datos del environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "Values",
-        authDomain: "Values",
-        projectId: "Values",
-        storageBucket: "Values",
-        messagingSenderId: "Values",
-        appId: "Values"
+        apiKey: "AIzaSyCI6-gAiH6WosNkS1Qlf576bPp8x1mSofM",
+        authDomain: "reparaciones-david.firebaseapp.com",
+        projectId: "reparaciones-david",
+        storageBucket: "reparaciones-david.firebasestorage.app",
+        messagingSenderId: "968829812440",
+        appId: "1:968829812440:web:c475d60e177dedb4b161ad",
+        measurementId: "G-CM9LDCPR4G"
     }
   };
   
